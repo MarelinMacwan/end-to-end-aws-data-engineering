7edcc271df91d519d7a8df86349d6b646edfa837
MARELIN MACWAN <marelinmacwan1999@gmail.com>
2026-01-10 20:35:59 -0500

    update query

--- analytics/business_queries.sql
+++ analytics/business_queries.sql
@@ -1,17 +1,28 @@
--- Monthly revenue
+-- Monthly Revenue
 SELECT
-    DATE_TRUNC('month', order_date) AS month,
-    SUM(revenue) AS total_revenue
-FROM analytics.fact_orders
+    DATE_TRUNC('month', order_purchase_timestamp::timestamp) AS month,
+    SUM(revenue::numeric) AS total_revenue
+FROM analytics.fact_orders_stage1
 GROUP BY 1
 ORDER BY 1;
 
--- Top products
+-- Top Categories
 SELECT
-    p.product_category,
-    SUM(f.revenue) AS revenue
-FROM analytics.fact_orders f
-JOIN analytics.dim_products p
-ON f.product_id = p.product_id
-GROUP BY 1
-ORDER BY revenue DESC;
+    product_category_name,
+    SUM(revenue::numeric) AS revenue
+FROM analytics.fact_orders_stage1
+GROUP BY product_category_name
+ORDER BY revenue DESC
+LIMIT 10;
+
+-- Customer-Level Revenue
+SELECT
+    customer_id,
+    SUM(revenue::numeric) AS total_revenue
+FROM analytics.fact_orders_stage1
+GROUP BY customer_id
+ORDER BY total_revenue DESC
+LIMIT 20;
+
+
+
